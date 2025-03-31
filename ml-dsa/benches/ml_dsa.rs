c9a2c5496bda6623294b40d7c610dde3a6fef56b
Jørgen Kristensen <jk@bitnor.org>
2025-03-31 10:09:26 +0200

    ml-dsa: measure cycles and use batched input gen

--- ml-dsa/benches/ml_dsa.rs
+++ ml-dsa/benches/ml_dsa.rs
@@ -1,4 +1,6 @@
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::measurement::Measurement;
+use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
+use criterion_cycles_per_byte::CyclesPerByte;
 use hybrid_array::{Array, ArraySize};
 use ml_dsa::{B32, KeyGen, MlDsa65, Signature, SigningKey, VerifyingKey};
 use rand::CryptoRng;
@@ -9,56 +11,100 @@ pub fn rand<L: ArraySize, R: CryptoRng + ?Sized>(rng: &mut R) -> Array<u8, L> {
     val
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn criterion_benchmark<M: Measurement>(c: &mut Criterion<M>) {
     let mut rng = rand::rng();
-    let xi: B32 = rand(&mut rng);
-    let m: B32 = rand(&mut rng);
-    let ctx: B32 = rand(&mut rng);
 
-    let kp = MlDsa65::key_gen_internal(&xi);
-    let sk = kp.signing_key();
-    let vk = kp.verifying_key();
-    let sig = sk.sign_deterministic(&m, &ctx).unwrap();
-
-    let sk_bytes = sk.encode();
-    let vk_bytes = vk.encode();
-    let sig_bytes = sig.encode();
+    let mut ml_dsa = c.benchmark_group("ML-DSA");
 
     // Key generation
-    c.bench_function("keygen", |b| {
-        b.iter(|| {
-            let kp = MlDsa65::key_gen_internal(&xi);
-            let _sk_bytes = kp.signing_key().encode();
-            let _vk_bytes = kp.verifying_key().encode();
-        })
+    ml_dsa.bench_function("keygen", |b| {
+        b.iter_batched(
+            || {
+                let xi: B32 = rand(&mut rng);
+                xi
+            },
+            |xi| {
+                let kp = MlDsa65::key_gen_internal(&xi);
+                let _sk_bytes = kp.signing_key().encode();
+                let _vk_bytes = kp.verifying_key().encode();
+            },
+            criterion::BatchSize::SmallInput,
+        );
     });
 
     // Signing
-    c.bench_function("sign", |b| {
-        b.iter(|| {
-            let sk = SigningKey::<MlDsa65>::decode(&sk_bytes);
-            let _sig = sk.sign_deterministic(&m, &ctx);
-        })
+    ml_dsa.bench_function("sign", |b| {
+        b.iter_batched(
+            || {
+                let xi: B32 = rand(&mut rng);
+                let m: B32 = rand(&mut rng);
+                let ctx: B32 = rand(&mut rng);
+
+                let kp = MlDsa65::key_gen_internal(&xi);
+                let sk = kp.signing_key();
+
+                let sk_bytes = sk.encode();
+                (sk_bytes, m, ctx)
+            },
+            |(sk_bytes, m, ctx)| {
+                let sk = SigningKey::<MlDsa65>::decode(&sk_bytes);
+                sk.sign_deterministic(&m, &ctx)
+            },
+            BatchSize::SmallInput,
+        );
     });
 
     // Verifying
-    c.bench_function("verify", |b| {
-        b.iter(|| {
-            let vk = VerifyingKey::<MlDsa65>::decode(&vk_bytes);
-            let sig = Signature::<MlDsa65>::decode(&sig_bytes).unwrap();
-            let _ver = vk.verify_with_context(&m, &ctx, &sig);
-        })
+    ml_dsa.bench_function("verify", |b| {
+        b.iter_batched(
+            || {
+                let xi: B32 = rand(&mut rng);
+                let m: B32 = rand(&mut rng);
+                let ctx: B32 = rand(&mut rng);
+
+                let kp = MlDsa65::key_gen_internal(&xi);
+                let sk = kp.signing_key();
+                let vk = kp.verifying_key();
+                let sig = sk.sign_deterministic(&m, &ctx).unwrap();
+
+                let vk_bytes = vk.encode();
+                let sig_bytes = sig.encode();
+                (vk_bytes, sig_bytes, m, ctx)
+            },
+            |(vk_bytes, sig_bytes, m, ctx)| {
+                let vk = VerifyingKey::<MlDsa65>::decode(&vk_bytes);
+                let sig = Signature::<MlDsa65>::decode(&sig_bytes).unwrap();
+                vk.verify_with_context(&m, &ctx, &sig)
+            },
+            BatchSize::SmallInput,
+        );
     });
 
     // Round trip
-    c.bench_function("round_trip", |b| {
-        b.iter(|| {
-            let kp = MlDsa65::key_gen_internal(&xi);
-            let sig = kp.signing_key().sign_deterministic(&m, &ctx).unwrap();
-            let _ver = kp.verifying_key().verify_with_context(&m, &ctx, &sig);
-        })
+    ml_dsa.bench_function("round_trip", |b| {
+        b.iter_batched(
+            || {
+                let xi: B32 = rand(&mut rng);
+                let m: B32 = rand(&mut rng);
+                let ctx: B32 = rand(&mut rng);
+
+                (xi, m, ctx)
+            },
+            |(xi, m, ctx)| {
+                let kp = MlDsa65::key_gen_internal(&xi);
+                let sig = kp.signing_key().sign_deterministic(&m, &ctx).unwrap();
+                kp.verifying_key().verify_with_context(&m, &ctx, &sig)
+            },
+            BatchSize::SmallInput,
+        );
     });
+
+    ml_dsa.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion::criterion_group!(
+    name = benches;
+    config = criterion::Criterion::default().with_measurement(CyclesPerByte);
+    targets = criterion_benchmark
+);
 criterion_main!(benches);
